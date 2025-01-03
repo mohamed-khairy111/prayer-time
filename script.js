2baf0180cb75846f6e24183147d83ddb39098f6e
mohamed khairy <mohamedkhairy0101@gmail.com>
2025-01-03 08:12:44 +0200

    added times for other cities

--- script.js
+++ script.js
@@ -23,37 +23,163 @@ let EG_KN = {
   city: 'Qinā',
 };
 
-let city = document.getElementById('selection').value;
+let selection = document.getElementById('selection');
 let city_name = document.getElementById('city-name');
 let date = document.getElementById('date');
 
-console.log(city);
+selection.addEventListener('change', () => {
+  let city = selection.value;
+  if (city == 'Kafr ash Shaykh') {
+    city_name.innerHTML = city;
 
-if (city == 'Kafr ash Shaykh') {
-  city_name.innerHTML = city;
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_KFS,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
 
-  axios
-    .get('http://api.aladhan.com/v1/timingsByCity', {
-      params: EG_KFS,
-    })
-    .then(function (response) {
-      const timings = response.data.data.timings;
-      FillTimeForPrayer('fajr-time', timings.Fajr);
-      FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
-      FillTimeForPrayer('Asr-time', timings.Asr);
-      FillTimeForPrayer('Maghrib-time', timings.Maghrib);
-      FillTimeForPrayer('Isha-time', timings.Isha);
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (city == 'Ad Daqahlīyah') {
+    city_name.innerHTML = city;
 
-      const weekDay = response.data.data.date.gregorian.weekday.en;
-      const DayNum = response.data.data.date.gregorian.day;
-      const month = response.data.data.date.gregorian.month.en;
-      date.innerHTML = `${weekDay} ${DayNum} ${month}`;
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_DK,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
+
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (city == 'Al Baḩr al Aḩmar') {
+    city_name.innerHTML = city;
+
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_BA,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
+
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (city == 'Al Buḩayrah') {
+    city_name.innerHTML = city;
+
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_BH,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
+
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (city == 'Al Fayyūm') {
+    city_name.innerHTML = city;
+
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_FYM,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
+
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  if (city == 'Qinā') {
+    city_name.innerHTML = city;
+
+    axios
+      .get('http://api.aladhan.com/v1/timingsByCity', {
+        params: EG_KN,
+      })
+      .then(function (response) {
+        const timings = response.data.data.timings;
+        FillTimeForPrayer('fajr-time', timings.Fajr);
+        FillTimeForPrayer('Dhuhr-time', timings.Dhuhr);
+        FillTimeForPrayer('Asr-time', timings.Asr);
+        FillTimeForPrayer('Maghrib-time', timings.Maghrib);
+        FillTimeForPrayer('Isha-time', timings.Isha);
+
+        const weekDay = response.data.data.date.gregorian.weekday.en;
+        const DayNum = response.data.data.date.gregorian.day;
+        const month = response.data.data.date.gregorian.month.en;
+        date.innerHTML = `${weekDay} ${DayNum} ${month}`;
+        city_name.innerHTML = city;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+});
 
 const FillTimeForPrayer = (id, time) => {
   document.getElementById(id).innerHTML = time;
